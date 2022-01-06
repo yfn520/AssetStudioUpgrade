a01a5b2e82c4c7d31fa7094f91b2434a0adac3ba
yinfuning <250257961@qq.com>
2022-01-06 10:27:30 +0800

    add:支持AB比对

---
+++ AssetStudio-master/AssetStudio/SerializedType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssetStudio
+{
+    public class SerializedType
+    {
+        public int classID;
+        public bool m_IsStrippedType;
+        public short m_ScriptTypeIndex = -1;
+        public TypeTree m_Type;
+        public byte[] m_ScriptID = new byte[0]; //Hash128
+        public byte[] m_OldTypeHash = new byte[0]; //Hash128
+        public int[] m_TypeDependencies = new int[0];
+        public string m_KlassName;
+        public string m_NameSpace;
+        public string m_AsmName;
+    }
+}
